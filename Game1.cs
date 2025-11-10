44e23ccf679480459d301dd8267dea0bc847b207
Ava Manning <362579849@GoTVDSB.ca>
2025-11-10 09:35:28 -0500

    working

--- Game1.cs
+++ Game1.cs
@@ -9,7 +9,19 @@ namespace Image_out_of_Shapes
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        SpriteFont textFont;
+        Rectangle hotPinkRectangle;
+        Rectangle purpleRectangle;
+        Rectangle purpleCircle;
+        Rectangle triangle;
+
+        Rectangle window;
+
         Texture2D Texture;
+        Texture2D triangleTexture;
+        Texture2D pinkRectangleTexture;
+        Texture2D circleTexture;
+        Texture2D purpleRectangleTexture;
 
         public Game1()
         {
@@ -21,6 +33,16 @@ namespace Image_out_of_Shapes
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 500);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            // x y, width, height
+            purpleRectangle = new Rectangle(0, 0, 0, 0);
+            hotPinkRectangle = new Rectangle(275, 200, 250, 100);
+            triangle = new Rectangle();
+            purpleCircle = new Rectangle(455, 125, 120, 120);
 
             base.Initialize();
         }
@@ -30,6 +52,12 @@ namespace Image_out_of_Shapes
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            pinkRectangleTexture = Content.Load<Texture2D>("hotPinkRectangle");
+            triangleTexture = Content.Load<Texture2D>("triangle");
+            circleTexture = Content.Load<Texture2D>("purpleCircle");
+            purpleRectangleTexture = Content.Load<Texture2D>("purpleRectangle");
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -47,6 +75,17 @@ namespace Image_out_of_Shapes
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+
+            _spriteBatch.Draw(pinkRectangleTexture, hotPinkRectangle, Color.White);
+            _spriteBatch.Draw(triangleTexture, triangle, Color.White);
+            _spriteBatch.Draw(purpleRectangleTexture, purpleRectangle, Color.White);
+            _spriteBatch.Draw(circleTexture, purpleCircle, Color.White);
+            
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
